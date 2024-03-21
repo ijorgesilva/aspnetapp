eeda8e67f80f947d33f059a507be44aaf4e71a10
Jorge Silva <jsilva@costargroup.com>
2024-03-21 11:25:26 -0400

    Adding Cors

--- app/aspnetapp/API/Program.cs
+++ app/aspnetapp/API/Program.cs
@@ -14,16 +14,18 @@ builder.Services.AddDbContext<DataContext>(opt =>
   opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-builder.Services.AddCors();
 
 app.UseHttpsRedirection();
 
---
+++ app/aspnetapp/API/README.md
@@ -0,0 +1,2 @@
+Run app
+`dotnet run`
